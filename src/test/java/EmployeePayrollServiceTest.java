370b565f12f33c156f10a6e9ec937e5bc9ddd2e7
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-11 22:39:24 +0530

    Added: write, print and count function in Employee File Io service

---
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EmployeePayrollServiceTest {
+	
+	@Test
+	public void givenEmployees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] empArray = {
+				new EmployeePayrollData(1, "Anik Chowdhury", 100000.0),
+				new EmployeePayrollData(1, "Avijit Chowdhury", 200000.0),
+				new EmployeePayrollData(1, "Arinjit Chowdhury", 300000.0),
+		};
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(empArray));
+		employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
+}
